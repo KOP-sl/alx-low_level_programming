db61548ae92cdbd6cf8b40fa9c5c3e3265619bd2
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 <kelvinpitt640@gmail.com>
2023-02-19 22:36:21 +0000

    0-positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
 
-	if (n > 0)
+	if (n > 5)
 	{
-		printf("%d is positive\n", n);
+		printf("last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
-	else if (n == 0)
+	else if (lastd == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("last digit of %d is %d and is 0\n", n, lastd);
 	}
-	else
+	else if (lastd < 6 && lastd != 0)
 	{
-		printf("%d is negative\n" n);
+		printf("last digit of %d is %d and is less than 6and not 0\n", n, lastd);
 	}
 	return (0);
 }
