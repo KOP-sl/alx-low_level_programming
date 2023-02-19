4387a6d07bd262c933caf9eb26aa851d6b87c4e9
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 <kelvinpitt640@gmail.com>
2023-02-19 22:11:56 +0000

    9-print_

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - prints all combination of sing digits
- * Return: Always 0
+ * Return: Always 0 (success)
  */
-int main(void)
-{
-	int n;
 
-	for (n = 48; n < 58; n++)
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(d);
+		if (d != '9')
 		{
-			putchar(',');
-			putchar(' ');
+		putchar(',');
+		putchar(' ');
+		}
 		}
 	putchar('\n');
 	return (0);
-	}
+}
