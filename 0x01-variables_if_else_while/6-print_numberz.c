dea50676a8f4f17d55a24bc979123af5e6cab0ba
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 <kelvinpitt640@gmail.com>
2023-02-19 21:09:34 +0000

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints single digits in base 10
- * Return: Anaways 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -10,6 +10,8 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putcher((num % 10) + '0');
+
 	putcher('\n');
+
 	return (0);
 }
