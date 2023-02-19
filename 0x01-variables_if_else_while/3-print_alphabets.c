72c39cd34d768074d1570e3e5ab4d458e5e75725
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 <kelvinpitt640@gmail.com>
2023-02-19 20:14:30 +0000

    4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,12 +3,14 @@
 /**
  * main - print alpha in lower case and upper case
  * Retrun: Always 0
+ * Description: 'The programs description'
  */
 int main(void)
 {
+	int ch;
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z' ch++)
 		putchar(ch);
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
