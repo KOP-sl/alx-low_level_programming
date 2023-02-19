e9d36f6b47c79adbf933372a029a174c7c9369d1
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 <kelvinpitt640@gmail.com>
2023-02-19 17:12:09 +0000

    101-quote.c1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - Entry
- * Return: 1
+ /**main - prints "and that piece of art is useful" . Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ * Return: Always 1.
  */
 int main(viod)
+
 {
-	write(1, and that piece of art is useful" - Dora Korpar, 2015-10-19,\n" 59);
-	return (1);
+write(2, "and that piece of art is useful\"   Dora Korpar, 2015-10-19,\n" 59);
+return (1);
 }
