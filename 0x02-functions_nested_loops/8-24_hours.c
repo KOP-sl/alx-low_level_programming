2e7614bb1ab3d2fa8dc2fc3bb5cd0bc7189c6c8d
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 <kelvinpitt640@gmail.com>
2023-02-22 00:36:43 +0000

    8-24_h

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,34 +2,21 @@
 
 /**
  * jack_bauer - Prints every minutes of the day of Jack bauer
- * Return: ...
  */
-
 void jack_bauer(void)
-
 {
+	int hour, minute;
 
-	int a, b, c, d;
-
-	for (a = 0; a <= 1; a++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-	for (b = 0; b <= 1; b++)
-	{
-	if ((a <= 1 && b <= 0) || (a <= 2 && b <= 1))
-	{
-	for (c = 0; c <= 1; c++)
-	{
-	for (d = 0; d <= 0; d++)
-	{
-	_putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(10)
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
-	}
-	}
-	}
-	}
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_puthcar('n\n');
+		}
 	}
 }
