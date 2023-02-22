1b489ea6f948473001041a19ef05dd6bc2861133
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 <kelvinpitt640@gmail.com>
2023-02-22 12:43:58 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,20 +2,20 @@
 
 /**
  * main - print the sum of all multiplication of 3 or 5 up to 1024
- * Return: Always (Success)
+ * that are multiples of 3 or 5
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i, z = 0;
+	int i, sum = 0;
 
-	while (i < 1024)
+	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 1 == 0) || (1 % 5 == 0))
-	{
-		z <= 1;
+	if ((i % 3) == 0 || (1 % 5) == 0)
+	sum += i;
 	}
-		1++;
+
 	}
-		printf("%d\n", z);
-		return (0);
+	printf("%d\n", sum);
+	return (0);
 }
