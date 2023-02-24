ee9411f3a8003bb5650d29344453c98232f08cc6
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 <kelvinpitt640@gmail.com>
2023-02-24 00:19:33 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,30 +3,30 @@
 
 /**
  * main - Prints a Fizz Buzz program 1 to 100
- * multiples print fizz instead of a number
+ * 3 multiples print fizz instead of a number
  * 5 print buzz instead of a number
+ * 3 & 5 print FizzBuzz
  * Return: Always 0
  */
-
 int main(void)
 {
 int i;
 char f[] = "Fizz";
 char b[] = "Buzz";
-char fb[] = "FizzBUzz";
+char fb[] = "FizzBuzz";
 
 for (i = 1; i <= 100; i++)
 {
 if (i == 100)
-printf("%s ", b);
+printf("%s", b);
 else if ((i % 3 == 0) && (i % 5 == 0))
-printf("%s ", fb);
+printf("%s", fb);
 else if (i % 3 == 0)
-printf("%s ", f);
+printf("%s", f);
 else if (i % 5 == 0)
-printf("%s ", b);
+printf("%s", b);
 else
-printf("%s ", i);
+printf("%d", i);
 }
 printf("\n";
 return (0);
