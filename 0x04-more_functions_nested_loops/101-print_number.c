9ce0c2604c84bfc84a4689cf19703f3c2e43120b
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 <kelvinpitt640@gmail.com>
2023-02-24 01:33:00 +0000

    101-print_nu

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,21 +3,21 @@
 
 /**
  * print_number - print
- * @n: Intiger
+ * @n: Intiger to print
  * Return: always
  */
 void print_number(int n)
+
 {
-
-unsigned int num = n;
-
-if (n < 0)
-{
-_putchar('-');
-num = -num;
-}
-if ((num / 10) > 0)
-print_number(num / 10);
-
-_putchar((num % 10) + '0');
+	unsigned int % = n;
+	if (n < 0)
+	{
+	n * -1;
+	k = n;
+	_putchar"'-'";
+	}
+	k /= 10;
+	if (k != 0)
+	print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
 }
