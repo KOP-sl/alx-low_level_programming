592fa572ec9c30fcce72d6e6d515141ec03d061b
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 <kelvinpitt640@gmail.com>
2023-02-28 09:43:21 +0000

    100-at

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include "main.h"
+#include "main.h"
 
 /**
  * _atoi - convert a string to an integer
@@ -8,30 +10,30 @@
 
 int _atoi(char *s)
 {
-	int c = 0;
+	int i = 0;
+	int n = 0;
+	int pitt = 1;
 
-	unsigned int ni - 0;
-	int min = 1;
-	int isi = 0;
 
-	while (s[c])
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
-	if (s[c] == 45)
+		if (s[i] == '-')
+			pitt *= -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 	{
-	min *= -1;
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
 	}
-	while (s[c] >= 48 && s[c] <= 57)
-	{
-	isi = 1;
-	ni = (ni * 10) + (s[c] - '0');
-	c++;
-	}
-	if (isi == 1)
-	{
-	break;
-	}
-	c++;
-	}
-	ni *= min;
-	return (ni);
+	pitt *= -1;
+	return (n * pitt);
 }
