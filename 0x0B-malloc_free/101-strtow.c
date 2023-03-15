8021aab3fc7553505a4c694d9bc20b7dd343a15b
KOP-sl <kelvinpitt640@gmail.com>
2023-03-15 14:34:02 +0000

    last task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -34,7 +34,7 @@ int count_words(char *str)
 	for (index = 0; *(str + index); index++)
 		len++;
 
-	for (index + 0; index < len; index++)
+	for (index = 0; index < len; index++)
 	{
 		if (*(str + index) != ' ')
 		{
